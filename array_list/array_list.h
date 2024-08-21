7aecdab7a681e4af7ed8ee631c20f2cc2a3570a1
Daniel Karnofel <91570684+danielkarnofel@users.noreply.github.com>
2024-08-20 19:11:40 -0700

    Updated array_list.h documentation

--- array_list/array_list.h
+++ array_list/array_list.h
@@ -3,62 +3,105 @@
 
 #include <stdbool.h>
 
-typedef int Element;
+/* A dynamically expanding, array based implentation of an ordered list. */ 
+
+#define Element int
+#define NULL_ELEMENT 0
+
 typedef struct array_list_t *ArrayList;
 
 /**********************************************************
- * create: 
+ * Creates a new array list.
+ * Params: initial_capacity - initial storage capacity of 
+ *         the array list; the list will be expanded if
+ *         the capacity is exceeded
+ * Return: an array_list_t pointer of type ArrayList
  **********************************************************/
-ArrayList create(int initial_capacity);
+ArrayList al_create(int initial_capacity);
 
 /**********************************************************
- * destroy: 
+ * Destroys the given array list.
+ * Params: list - an array_list_t pointer of type ArrayList
+ * Return: void
  **********************************************************/
-void destroy(ArrayList list);
+void al_destroy(ArrayList list);
 
 /**********************************************************
- * add_to_list: 
+ * Adds a new element to the array list at the specified
+ * position. Shifts the other elements to make room.
+ * Params: list - an array_list_t pointer of type ArrayList
+ *         index - the index at which to add the element
+ *         e - element to be added to the list
+ * Return: true if the element was added successfully and 
+ *         false otherwise
  **********************************************************/
-bool add_to_list(ArrayList list, int index, Element e);
+bool al_add(ArrayList list, int index, Element e);
 
 /**********************************************************
- * remove_from_list:
+ * Removes an element at the specified position. Shifts
+ * the other elements to fill the space.
+ * Params: list - an array_list_t pointer of type ArrayList
+ *         index - the index at which to remove the element
+ * Return: the removed element if removed successfully, or 
+ *         the NULL_ELEMENT defined above if unable to
+ *         remove
  **********************************************************/
-Element remove_from_list(ArrayList list, int index);
+Element al_remove(ArrayList list, int index);
 
 /**********************************************************
- * set:
+ * Replaces the element at the specified position with a 
+ * new element.
+ * Params: list - an array_list_t pointer of type ArrayList
+ *         index - the index at which to replace
+ *         e - the new element to replace the original
+ * Return: the previous value if set successfully, or the 
+ *         NULL_ELEMENT defined above if unable to set
  **********************************************************/
-Element set(ArrayList list, int index, Element e);
+Element al_set(ArrayList list, int index, Element e);
 
 /**********************************************************
- * get:
+ * Gets the element at the specified position.
+ * Params: list - an array_list_t pointer of type ArrayList
+ *         index - the index at which to get the element
+ * Return: the element at the specified position, or the 
+ *         NULL_ELEMENT defined above if unable to get
  **********************************************************/
-Element get(ArrayList list, int index);
+Element al_get(ArrayList list, int index);
 
 /**********************************************************
- * get_index:
+ * Gets the index of an element.
+ * Params: list - an array_list_t pointer of type ArrayList
+ *         e - the element to search for
+ * Return: the index of the element if found, or -1 if the 
+ *         element is not found
  **********************************************************/
-int get_index(ArrayList list, Element e);
+int al_index_of(ArrayList list, Element e);
 
 /**********************************************************
- * size:
+ * Gets the number of elements in the list
+ * Params: list - an array_list_t pointer of type ArrayList
+ * Return: the number of elements in the list
  **********************************************************/
-int size(ArrayList list);
+int al_size(ArrayList list);
 
 /**********************************************************
- * is_empty:
+ * Determines if the list is empty
+ * Params: list - an array_list_t pointer of type ArrayList
+ * Return: true if the list is empty and false otherwise
  **********************************************************/
-bool is_empty(ArrayList list);
+bool al_is_empty(ArrayList list);
 
 /**********************************************************
- * clear:
+ * Removes all elements from the list, setting the value
+ * to the NULL_ELEMENT defined above.
+ * Params: list - an array_list_t pointer of type ArrayList
  **********************************************************/
-void clear(ArrayList list);
+void al_clear(ArrayList list);
 
 /**********************************************************
- * print_list:
+ * Prints the list to the console.
+ * Params: list - an array_list_t pointer of type ArrayList
  **********************************************************/
-void print_list(ArrayList list);
+void al_print(ArrayList list);
 
 #endif
